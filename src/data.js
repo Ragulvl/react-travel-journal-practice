597b9cfe9a5d99d8665d00a23668ab77fd2c1c38
ragulvl <ragulkamelash@gmail.com>
2025-01-07 16:45:00 +0530

    Add travel data array

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    title: "Mount Fuji",
+    country: "Japan",
+    mapLink: "https://maps.google.com",
+    date: "12 Jan, 2021 - 24 Jan, 2021",
+    description: "Mount Fuji is the tallest mountain in Japan.",
+    image: "https://source.unsplash.com/WLxQvbMyfas"
+  },
+  {
+    id: 2,
+    title: "Sydney Opera House",
+    country: "Australia",
+    mapLink: "https://maps.google.com",
+    date: "27 May, 2021 - 8 Jun, 2021",
+    description: "The Sydney Opera House is a multi-venue performing arts centre.",
+    image: "https://source.unsplash.com/JmuyB_LibRo"
+  },
+  {
+    id: 3,
+    title: "Geirangerfjord",
+    country: "Norway",
+    mapLink: "https://maps.google.com",
+    date: "01 Oct, 2021 - 18 Nov, 2021",
+    description: "The Geiranger Fjord is a fjord in the Sunnmøre region.",
+    image: "https://source.unsplash.com/3PeSjpLVtLg"
+  }
+]
+
